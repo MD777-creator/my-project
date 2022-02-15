03cc2a6512778a9888382115a238f9c2d03f57f1
GuWu <3111018605@qq.com>
2022-02-16 00:02:27 +0800

    初始化项目

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,17 +1,31 @@
 <template>
-  <div>{{ iid }}</div>
+  <div id="detail">
+    <detail-nav-bar />
+    <detail-swiper :top-images="topImages" />
+  </div>
 </template>
 
 <script>
+import DetailNavBar from "./childComps/DetailNavBar.vue";
+import DetailSwiper from "./childComps/DetailSwiper.vue";
+import { getDetail } from "../../network/detail";
 export default {
   name: "Detail",
+  components: {
+    DetailNavBar,
+    DetailSwiper,
+  },
   data() {
     return {
       iid: null,
+      topImages: [],
     };
   },
   created() {
     this.iid = this.$route.params.iid;
+    getDetail(this.iid).then((res) => {
+      this.topImages = res.result.itemInfo.topImages;
+    });
   },
 };
 </script>
