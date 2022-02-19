616a81d0874a63a4d6cc89890370c767d93690ff
GuWu <3111018605@qq.com>
2022-02-19 23:55:07 +0800

    初始化项目

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,12 +1,50 @@
 <template>
-  <div class="catego"></div>
+  <div id="category">
+    <nav-bar class="nav-bar">
+      <template #center>
+        <div>商品分类</div>
+      </template>
+    </nav-bar>
+    <div class="content">
+      <tab-menu :categories="categories" @selectItem="selectItem" />
+    </div>
+  </div>
 </template>
 
 <script>
+import NavBar from "../../components/common/navbar/NavBar.vue";
+import TabMenu from "./ChildComps/TabMenu.vue";
 export default {
   name: "Category",
+  components: {
+    NavBar,
+    TabMenu,
+  },
+  data() {
+    return {
+      categories: [],
+    };
+  },
+  methods: {
+    selectItem() {},
+  },
 };
 </script>
 
 <style scoped>
+#category {
+  height: 100vh;
+}
+.nav-bar {
+  background-color: var(--color-tint);
+  font-weight: 700;
+  color: #fff;
+}
+.content {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 44px;
+  bottom: 49px;
+}
 </style>
