1b6a48b42d62889202e9829651f67b516b913c1a
GuWu <3111018605@qq.com>
2022-02-19 19:28:44 +0800

    初始化项目

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,23 +1,93 @@
 <template>
-  <div>
-    <scroll class="content"></scroll>
+  <div id="profile">
+    <nav-bar>
+      <template #center>
+        <div>优衣商城</div>
+      </template>
+    </nav-bar>
+    <user-info />
+    <section class="account">
+      <div class="account-item">
+        <div class="number"><span class="balance">0.00</span>元</div>
+        <div class="account-info">我的余额</div>
+      </div>
+      <div class="account-item">
+        <div class="number"><span class="balance">0</span>个</div>
+        <div class="account-info">我的优惠</div>
+      </div>
+      <div class="account-item">
+        <div class="number"><span class="balance">0</span>分</div>
+        <div class="account-info">我的积分</div>
+      </div>
+    </section>
+    <list-view :list-data="orderList" class="order-list" />
+    <list-view :list-data="serviceList" class="service-list" />
   </div>
 </template>
 
 <script>
-import Scroll from "../../components/common/scroll/Scroll.vue";
+import NavBar from "../../components/common/navbar/NavBar.vue";
+import UserInfo from "./ChildComps/UserInfo.vue";
+import ListView from "./ChildComps/ListView.vue";
 export default {
   name: "Profile",
   components: {
-    Scroll,
+    NavBar,
+    UserInfo,
+    ListView,
+  },
+  data() {
+    return {
+      orderList: [
+        { icon: "#order", iconColor: "#ff8198", info: "我的消息" },
+        { icon: "#point", iconColor: "#fc7b53", info: "积分商城" },
+        { icon: "#vip", iconColor: "#ffc636", info: "会员卡" },
+      ],
+      serviceList: [
+        { icon: "#service", iconColor: "#ff8198", info: "我的购物车" },
+        { icon: "#download", iconColor: "#ff8198", info: "下载购物APP" },
+      ],
+    };
   },
 };
 </script>
 
 <style scoped>
-.content {
-  height: 300px;
-  background-color: red;
-  overflow: hidden;
+#profile {
+  background-color: #f2f2f2;
+}
+.nav-bar {
+  background: var(--color-tint);
+  font-weight: 700;
+  color: #fff;
+}
+.account {
+  display: flex;
+}
+.account-item {
+  width: 100%;
+  background-color: #fff;
+  margin-right: 1px;
+  text-align: center;
+}
+.account-item:last-of-type {
+  margin-right: 0;
+}
+.account-item {
+  color: #666;
+  font-size: 13px;
+  padding: 18px;
+}
+.account-item .balance {
+  font-size: 24px;
+  font-weight: 700;
+  color: #ff5f3e;
+}
+.account-info {
+  margin-top: 6px;
+}
+.order-list,
+.service-list {
+  margin-top: 12px;
 }
 </style>
