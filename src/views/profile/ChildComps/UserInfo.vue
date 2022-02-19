586e12a53bf7f6b977ce60568788ac9af3930aaf
GuWu <3111018605@qq.com>
2022-02-19 22:45:44 +0800

    初始化项目

--- src/views/profile/ChildComps/UserInfo.vue
+++ src/views/profile/ChildComps/UserInfo.vue
@@ -2,37 +2,22 @@
   <div id="user-info">
     <a href="#" class="clear-fix">
       <slot name="user-icon">
-        <svg class="privateImage-svg left">
-          <use
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xlink:href="#avatar-default"
-          ></use>
-        </svg>
+        <div class="privateImage-svg left">
+          <img src="@/assets/img/profile/user.svg" alt="" />
+        </div>
       </slot>
       <div class="login-info left">
         <slot name="user-nickname">
           <div>登录/注册</div>
         </slot>
         <div class="phone">
-          <span>
-            <svg data-v-735ff1be="" fill="#fff" class="icon-mobile">
-              <use
-                data-v-735ff1be=""
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                xlink:href="#mobile"
-              ></use>
-            </svg>
-          </span>
+          <img src="@/assets/img/profile/phone.svg" />
           <slot name="user-phone">暂无绑定手机号</slot>
         </div>
       </div>
-      <svg data-v-735ff1be="" fill="#fff" class="arrow-svg right">
-        <use
-          data-v-735ff1be=""
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          xlink:href="#arrow-right"
-        ></use>
-      </svg>
+      <div class="arrow-svg right">
+        <img src="@/assets/img/profile/turnRight.svg" alt="" />
+      </div>
     </a>
   </div>
 </template>
@@ -49,17 +34,18 @@ export default {
   padding: 15px;
   margin-top: -5px;
 }
-#user-info .privateImage-svg {
+#user-info .privateImage-svg img {
   width: 60px;
   height: 60px;
   background-color: #fff;
   border-radius: 30px;
+  border: 3px solid #fff;
 }
 .left {
   float: left;
 }
-#user-info .arrow-svg {
-  width: 11px;
+#user-info .arrow-svg img {
+  width: 22px;
   height: 22px;
   margin-top: 18px;
 }
@@ -74,11 +60,11 @@ export default {
   margin-left: 15px;
   font-weight: 300;
 }
-#user-info .login-info .phone .icon-mobile {
+#user-info .login-info .phone img {
   position: absolute;
-  width: 12px;
-  height: 18px;
-  left: -15px;
-  top: 0;
+  width: 20px;
+  height: 20px;
+  left: -20px;
+  top: -4px;
 }
 </style>
