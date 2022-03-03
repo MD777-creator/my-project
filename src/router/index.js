21d75c44084370395ad0d620e2c3f075c4125de0
GuWu <3111018605@qq.com>
2022-03-03 16:13:39 +0800

    faith

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,40 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
-
-export default new Router({
+Vue.use(Router);
+const VueRouter = new Router({
   routes: [
     {
-      path: '',
-      redirect: '/home'
+      path: "/",
+      redirect: "/home"
     },
     {
-      path: '/home',
+      path: "/home",
       component: () => import("../views/home/Home.vue")
     },
     {
-      path: '/category',
+      path: "/category",
       component: () => import("../views/category/Category.vue")
     },
     {
-      path: '/cart',
+      path: "/cart",
       component: () => import("../views/cart/Cart.vue")
     },
     {
-      path: '/profile',
+      path: "/profile",
       component: () => import("../views/profile/Profile.vue")
     },
     {
-      path: '/detail/:iid',
+      path: "/detail/:iid",
       component: () => import("../views/detail/Detail.vue")
-    },
+    }
   ],
   mode: "history"
-})
+});
+export default VueRouter;
+const originalPush = Router.prototype.push;
+Router.prototype.push = function push(location, resolve, reject) {
+  if (resolve || reject)
+    return originalPush.call(this, location, resolve, reject);
+  return originalPush.call(this, location).catch(err => err);
+};
